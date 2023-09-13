46b2f96442d852601727b0f3619d80780a98b9e7
MahmudBayram2000 <143312904+MahmudBayram2000@users.noreply.github.com>
2023-09-13 12:47:33 +0400

    my_second_casting_codes
    thank for your checking

---
+++ codes/javase/yenijava/src/casting.java
@@ -0,0 +1,62 @@
+public class casting {
+    public static void main(String[] args) {
+
+        short s=69;
+        byte  b=(byte)s;
+        System.out.println(b);
+
+        short sh=589;
+        byte  by=(byte)sh;
+        System.out.println(by);
+
+        long l=458;
+        int  i=(int)l;
+        System.out.println(i);
+
+        long lo=52523635483L;
+        int  in=(int)lo;
+        System.out.println(in);
+
+        double d=5632.6D;
+        float  f=(float)d;
+        System.out.println(f);
+
+        char c='D';
+        int inn=c;
+        System.out.println(inn);
+
+        int m=123;
+        char n=(char)m;
+        System.out.println(n);
+
+        int hey=55;
+        byte byt=(byte)hey;
+        System.out.println(byt);
+
+        short sha=636;
+        byte bal=(byte)sha;
+        System.out.println(bal);
+
+        long lay=458;
+        short shake=(short)lay;
+        System.out.println(shake);
+
+        long lag=92523635483L;
+        int ima=(int)lag;
+        System.out.println(ima);
+
+
+        double doub=65787.3D;
+        int maho=(int)doub;
+        System.out.println(maho);
+
+        char ch='q';
+        int id=(int)ch;
+        System.out.println(id);
+
+
+        int ik=266;
+        char ckk=(char)ik;
+        System.out.println(ckk);
+    }
+}
