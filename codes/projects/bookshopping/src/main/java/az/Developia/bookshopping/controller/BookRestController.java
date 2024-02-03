fa0c0e939b409fc7dae30b574c38ff74aae14ad3
MahmudBayram2000 <143312904+MahmudBayram2000@users.noreply.github.com>
2024-02-03 12:33:59 +0400

    find book by id

--- codes/projects/bookshopping/src/main/java/az/Developia/bookshopping/controller/BookRestController.java
+++ codes/projects/bookshopping/src/main/java/az/Developia/bookshopping/controller/BookRestController.java
@@ -1,14 +1,16 @@
 package az.Developia.bookshopping.controller;
 
-import java.awt.print.Book;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import az.Developia.bookshopping.dao.BookDAO;
+import az.Developia.bookshopping.model.Book;
 
 @RestController
 @RequestMapping(path="/books")
@@ -22,5 +24,9 @@ public class BookRestController {
 		return bookDAO.findAll();
 		
 	}
+	@GetMapping(path="/{id}")
+	public List<Book> findById(@PathVariable(name="id") Integer id){
+		return (List<Book>) bookDAO.findById(id).get();
 	}
+}
 
